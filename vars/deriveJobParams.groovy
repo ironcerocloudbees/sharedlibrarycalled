2f9480fe7095d9c3268cea986c7e40fb8faf7f40
Ignacio Roncero <61473809+ironcerocloudbees@users.noreply.github.com>
2020-11-11 14:51:38 +0100

    Update deriveJobParams.groovy

--- vars/deriveJobParams.groovy
+++ vars/deriveJobParams.groovy
@@ -2,7 +2,8 @@ def call() {
     properties([
         parameters([
             booleanParam(defaultValue: false, name: 'BuildAll', description: ''),
-            booleanParam(defaultValue: false, name: 'parameter1', description: '')
+            booleanParam(defaultValue: false, name: 'parameter1', description: ''),
+            choice(choices: ['environment1','environment2','environment3'], description: 'Which destination(s)?', name: 'environment')
         ])
     ])
 }
