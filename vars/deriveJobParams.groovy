0b4654f45a59a115ef0d6e4e09720291e7863d12
Ignacio Roncero <61473809+ironcerocloudbees@users.noreply.github.com>
2020-11-11 15:25:55 +0100

    Update deriveJobParams.groovy

--- vars/deriveJobParams.groovy
+++ vars/deriveJobParams.groovy
@@ -1,7 +1,7 @@
 def call() {
     properties([
         parameters([
-            choice(choices: 'environment1\nenvironment2\nenvironment3', description: 'Which destination(s)?', name: 'environment')
+            choice(choices: 'environment1\nenvironment2\nenvironment3\nenvironment4', description: 'Which destination(s)?', name: 'environment')
         ])
     ])
 }
